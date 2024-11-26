8a2594d7d12aa5f4e17ff06714b985f108c293af
bogathi reddy <bogathi.reddy@powerschool.com>
2024-11-26 15:01:11 +0530

    sample component created

---
+++ src/app/components/infocards.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Infocards = () => {
+  return (
+    <View>
+      <Text>My name is shanu</Text>
+      <Text>I'm 5 years old</Text>
+      <Text>Here is my X handle... nope</Text>
+      <Text>I love CS</Text>
+    </View>
+  )
+}
+
+export default Infocards
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import Infocards from './components/infocards'
 
 const index = () => {
   return (
-    <View>
-      <Text>index</Text>
-    </View>
-  )
+  
+      <Infocards/>
+    )
 }
 
 export default index
